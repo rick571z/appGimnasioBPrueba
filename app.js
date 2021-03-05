d3b894cacd4c4b91db6426ffa9c464f8ff1e09e1
Fire <wilmer_fi@hotmail.com>
2021-03-05 21:10:21 +0100

    se agrego la ruta clientes

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//RUTAS
+const clienteRouter = require('./routes/clientes');
+
 var app = express();
 
 // view engine setup
