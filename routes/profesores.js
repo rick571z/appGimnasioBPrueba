b2f5fab8256fa3ac68939e33931defccd01e561e
Fire <wilmer_fi@hotmail.com>
2021-03-06 01:35:34 +0100

    se agrego get para todos los profesores y para un nuevo profesor

--- routes/profesores.js
+++ routes/profesores.js
@@ -1,7 +1,13 @@
 //1.- se traer el router
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send('peticion get profesor....');
+});
 
+router.get('/new', (req, res) => {
+    res.send('peticion get new profesor....');
+});
 
 
 //2.- exportar
