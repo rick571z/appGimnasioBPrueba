85abc63b04b245cbb412eb14b29a6d5a3fe4cea7
Fire <wilmer_fi@hotmail.com>
2021-03-06 01:11:07 +0100

    creacion de los metodos get y post en el cliente que esta en routes

--- routes/clientes.js
+++ routes/clientes.js
@@ -6,7 +6,45 @@ const router = require('express').Router();
  */
 router.get('/', (req, res) => {
 
-    res.send('peticion get');
+    res.send('peticion get....');
+});
+
+
+/**
+ * 
+ */
+router.get('/new', (req, res) => {
+    res.send('peticion get new....');
+});
+
+/**
+ * 
+ */
+router.get('/edit/:idCliente', (req, res) => {
+    res.send('peticion get editar....');
+});
+
+/**
+ * 
+ */
+router.post('/create', (req, res) => {
+    console.log(req.body);
+    res.send('peticion post create cliente....');
+});
+
+
+/**
+ * 
+ */
+router.post('/update', (req, res) => {
+    res.send('peticion post update clientes....');
+});
+
+/**
+ * 
+ */
+router.get('/delete/:idCliente', (req, res) => {
+    res.send('peticon get delete cliente....');
 });
 
 
